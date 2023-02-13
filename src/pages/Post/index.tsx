e86c5c8e89bad7b0d1f9ee8dfdf62ddfbdc0637b
Gabriel Cesarino <61214104+gabrielCesarino@users.noreply.github.com>
2023-02-13 18:33:12 -0300

    style: post page style with react-markdown

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,10 +1,19 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
 import { api } from '../../api/api';
+import { Container, PostHeader, Iteractions, PostDetails, Content } from './styles';
+
+import githubIcon from '../../assets/github.svg';
+import socialIcon from '../../assets/social.svg';
+import buildIcon from '../../assets/build.svg';
+import redirectIcon from '../../assets/redirect.svg';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import remarkBreaks from 'remark-breaks';
 
 interface PostProps { 
 	body: string,
 	title: string,
+	html_url: string,
 }
 
 export function Post() {
@@ -17,7 +26,39 @@ export function Post() {
 	}, []);
 
 
-	return(
-		<p>{post?.body}</p>
+	console.log(post);
+	return (
+		post && 
+		<Container>
+			<PostHeader>
+				<Iteractions>
+					<NavLink to='/'>Voltar</NavLink>
+					<a href={post.html_url} target="_blank" rel="noreferrer">
+						Ver no github
+						<img src={redirectIcon} alt="Ver no Github" />
+					</a>
+				</Iteractions>
+				<h1>{post.title}</h1>
+				<PostDetails>
+					<span>
+						<img src={githubIcon} alt="" />
+					
+					</span>
+					<span>
+						<img src={socialIcon} alt="" />
+						Rocketseat
+					</span>
+					<span>
+						<img src={buildIcon} alt="" />
+					seguidores
+					</span>
+				</PostDetails>
+			</PostHeader>
+			<Content>
+				<ReactMarkdown remarkPlugins={[remarkBreaks]}>
+					{post.body}
+				</ReactMarkdown>
+			</Content>
+		</Container>
 	);
 }
--- src/pages/Post/styles.ts
+++ src/pages/Post/styles.ts
@@ -1,3 +1,91 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 54rem;
+    margin-top: -5rem;
+`;
+
+export const PostHeader = styled.header`
+    width: 100%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 2rem;
+
+    background-color: ${props => props.theme['base-profile']};
+    padding: 2rem;
+
+    border-radius: 10px;
+
+    & > img {
+        width: 9rem;
+        border-radius: 8px;
+    }
+
+    & > h1 {
+        font-size: 1.5rem;
+        color: ${props => props.theme['base-title']};
+    }
+`;
+
+export const Iteractions = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    & > a {
+        text-decoration: none;
+        text-transform: uppercase;
+        color: ${props => props.theme['base-brand']};
+        font-size: 0.875rem;
+        font-weight: bold;
+
+        display: flex;
+        gap: 0.5rem;
+
+        cursor: pointer;
+    }
+`;
+
+export const PostDetails = styled.div`
+    display: flex;
+    align-items: baseline;
+    gap: 2rem;
+    color: ${props => props.theme['base-subtitle']};
+
+    span {
+        display: flex;
+        align-items: center;
+    }
+
+    span > img {
+        margin-right: 0.25rem;
+        line-height: 0;
+    }
+`;
+
+export const Content = styled.div`
+    width: 50rem;
+
+    color: ${props => props.theme['base-text']};
+    margin: 2rem 0;
+
+    & > p {
+        line-height: 1.6;
+        
+        & + p {
+            margin-top: 1.5rem;
+        }
+
+        a {
+            text-decoration: none;
+            color: ${props => props.theme['base-brand']};
+            font-weight: bold;
+        }
+    }
+`;
